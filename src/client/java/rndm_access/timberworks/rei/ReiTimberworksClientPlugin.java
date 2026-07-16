12855b4223b988fbfeb8f24f3bbeb4292f3809ad
rndm_access <rndm.access.dev@gmail.com>
2026-07-15 23:37:38 -0600

    Added a transfer handler for the woodcuttter!

--- src/client/java/rndm_access/timberworks/rei/ReiTimberworksClientPlugin.java
+++ src/client/java/rndm_access/timberworks/rei/ReiTimberworksClientPlugin.java
@@ -3,12 +3,17 @@ package rndm_access.timberworks.rei;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
+import me.shedaniel.rei.api.client.registry.transfer.simple.SimpleTransferHandler;
 import me.shedaniel.rei.api.common.util.EntryStacks;
+import me.shedaniel.rei.plugin.common.BuiltinPlugin;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import rndm_access.timberworks.Timberworks;
 import rndm_access.timberworks.core.ModBlocks;
 import rndm_access.timberworks.core.ModRecipeTypes;
+import rndm_access.timberworks.menu.WoodcutterMenu;
 import rndm_access.timberworks.recipe.WoodcuttingRecipe;
 
 import java.util.Objects;
@@ -31,5 +36,15 @@ public class ReiTimberworksClientPlugin implements REIClientPlugin {
         for (RecipeHolder<WoodcuttingRecipe> recipe : recipes) {
             registry.add(new WoodcuttingDisplay(recipe));
         }
+    }
+
+    @Override
+    public void registerTransferHandlers(TransferHandlerRegistry registry) {
+        SimpleTransferHandler handler = SimpleTransferHandler.create(
+                WoodcutterMenu.class, // The class of your crafting menu
+                ReiIds.WOOD_CUTTING, // The category identifier of your display
+                new SimpleTransferHandler.IntRange(0, 1) // The range of recipe slots
+        );
+        registry.register(handler);
     }
 }
